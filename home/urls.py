08f7b65e7eba08d20a248173f09fd32e6c9520d2
John <pohui_stu@outlook.com>
2019-03-28 13:11:24 +0200

    carts

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home),
+    path('', views.ShowNewsList.as_view(), name="home"),
+    path('discount/<int:pk>/', views.NewsDetailView.as_view(), name='discount_detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Discount
-def home(request):
-    discount = Discount.objects.all()
-    return render(request, "home/home.html", {"discount":discount,
-    "title":"Discount",
-    })
+from django.views.generic import ListView, DetailView
+
+class ShowNewsList(ListView):
+    model = Discount
+    template_name = "home/home.html"
+    context_object_name = 'discount'
+    ordering = ['-date']
+    def get_context_data(self, **kwards):
+        ctx = super(ShowNewsList, self).get_context_data(**kwards)
+        ctx['title'] = 'Home'
+        return ctx
+
+class NewsDetailView(DetailView):
+    model = Discount
+    template_name = "home/discount_detail.html"
+    context_object_name = 'post'
+    def get_context_data(self, **kwards):
+        ctx = super(NewsDetailView, self).get_context_data(**kwards)
+        ctx['title'] = Discount.objects.filter(pk=self.kwargs['pk']).first()
+        return ctx
